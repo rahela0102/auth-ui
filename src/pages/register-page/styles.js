2ce205829cf61e6fb9e5d4e476f634f5956750d7
rahela02 <tutulearahela@yahoo.com>
2024-04-16 20:36:51 +0300

    added border color for register and login form

--- src/pages/register-page/styles.js
+++ src/pages/register-page/styles.js
@@ -15,7 +15,7 @@ export const styles = css`
     border-radius: 10px;
     width: 100%;
     padding: 20px 30px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    border: 1px solid #ff6200;
   }
 
   .create-account-text {
